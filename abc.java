4f07db30fd578dafa6838e1e5c55792beb824a8a
Ritesh <riteshpalo18@gmail.com>
2020-03-25 12:52:42 +0530

    class added

--- abc.java
+++ abc.java
@@ -1,8 +1,9 @@
+<<<<<<< Updated upstream
 import java.regex.*;
 import java.util.*;
 import java.io.*;
 import java.util.function.*;
-public class {
+public class Employee{
 	public static void main(String... args){
 		int a=10;
 	}
